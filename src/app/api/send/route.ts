9f367faf80e06f96f312c0ec636dc798e6d36682
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-12-23 03:41:12 +0500

    Changes made in emails

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -10,9 +10,8 @@ export async function POST() {
 
   try {
     const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["anassiddiqui0910@gmail.com"],
-      bcc: ["ayangfx.com@gmail.com"],
+      from: "onboarding@resend.dev",
+      to: ["ayangfx.com@gmail.com", "anassiddiqui0910@gmail.com"],
       subject: "Hello world",
       text: "Hi John,\nWelcome to Acme!",
       react: EmailTemplate({ firstName: "Anas Siddiqui" }),
